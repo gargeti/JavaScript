019055bc18f1c6ee4b2a2ca411d96451e0e1ae89
gargeti <138214793+gargeti@users.noreply.github.com>
2024-10-05 20:54:25 +0000

    changes done

---
+++ 01_basics/variables.js
@@ -0,0 +1,23 @@
+const accountId = 144343
+let accountEmail="garge@13gmail.com"
+var accountPassword="2323"
+accountCity="shamli"
+let accountState;
+
+// accountId=2
+
+accountEmail="tdgiuiuh@gmail.com"
+accountPassword="23323"
+accountCity="shimla"
+console.log(accountId);
+
+/* 
+1. {}=> scope
+2. for constant use only const and for variable use let only
+3. prefered not to use var bcz of issues in block scope and functional scope  
+4. javaScript is a safe language as without initialise the variable we can write
+the variable name but not a prefered way but can be used  
+5. if the value of the variable cant be defined so js considered it as undefined */
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
