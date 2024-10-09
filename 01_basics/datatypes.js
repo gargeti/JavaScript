109e5cba726809af1e98f8fcd3de99691bd5b446
gargeti <138214793+gargeti@users.noreply.github.com>
2024-10-09 09:57:09 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all js code as newer version
+// alter(3+3) we are using nodejs not browser
+
+
+ console.log(3+3);
+ console.log("eti garg");  //code readable
+// documents for syntax and information of javascript are tc39.es and mdn
+
+let name="Eti garg"
+let age =8
+let isloggedin = false
+
+// number =>2 to power53
+// bigint
+// string =>"",''
+// boolean => true/false
+// null=> standalone value
+// undefined => not value assigned
+// symbol =>uniqueness
+// object 
+
+
+
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof name);//string
+console.log(typeof isloggedin);//boolean
+console.log(typeof undefined);//undefined
