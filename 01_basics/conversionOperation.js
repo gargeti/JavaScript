b772375a9ce09001d007535a90213aad54275f63
gargeti <138214793+gargeti@users.noreply.github.com>
2024-10-10 17:38:02 +0000

    conversion of datatype

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,37 @@
+// let score=33
+//  console.log(typeof score);//number
+//  console.log(typeof (score));//number
+
+
+// let score="33"
+// let valueInNumber =Number (score)
+// console.log(typeof valueInNumber);//number
+// console.log(typeof (valueInNumber));//number
+//  while use name as a datatype function then it start with capital letter.
+
+
+// let score="3abe"
+// console.log(score);//3abe
+// console.log(typeof score);//string
+
+// let value =Number(score)
+// console.log(value);//NaN (special type of dt) as the combination of number and string so it is Not a Number
+// console.log(typeof value);//number
+
+// "33"=>33
+// "33abe"=>NaN
+// true=>1;false=>0
+
+let isLoggedIn =1
+// 0=> false; 1=> true
+// if empty string =>false
+// if string => true
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
+
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber); // 33
+console.log(typeof(stringNumber)); // string
