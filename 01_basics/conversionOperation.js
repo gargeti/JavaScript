8cf249e8d63e7c5efd0a3077f1916f0cf97219d1
gargeti <138214793+gargeti@users.noreply.github.com>
2024-10-11 16:43:19 +0000

    changes

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -3,6 +3,7 @@
 //  console.log(typeof (score));//number
 
 
+
 // let score="33"
 // let valueInNumber =Number (score)
 // console.log(typeof valueInNumber);//number
@@ -10,28 +11,60 @@
 //  while use name as a datatype function then it start with capital letter.
 
 
+
 // let score="3abe"
 // console.log(score);//3abe
 // console.log(typeof score);//string
+
+
 
 // let value =Number(score)
 // console.log(value);//NaN (special type of dt) as the combination of number and string so it is Not a Number
 // console.log(typeof value);//number
 
+
+
 // "33"=>33
 // "33abe"=>NaN
 // true=>1;false=>0
 
-let isLoggedIn =1
-// 0=> false; 1=> true
-// if empty string =>false
-// if string => true
-let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); // true
+
+// let isLoggedIn =1
+// // 0=> false; 1=> true
+// // if empty string =>false
+// // if string => true
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
 
 
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber); // 33
-console.log(typeof(stringNumber)); // string
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber); // 33
+// console.log(typeof(stringNumber)); // string
+
+
+
+
+// ****************************Operations**********************************************
+// let value=3
+// let negvalue=-value
+// console.log(negvalue);
+
+
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2/2);//1
+// console.log(2%2);//0
+// console.log(2^4);//6
+// console.log(2**4);//16
+
+
+
+// let str1="hello,"
+// let str2=" its me Eti Garg"
+// console.log(str1+str2);
+// console.log(typeof (str1+str2));
+// //hello, its me Eti Garg 
+// // string
