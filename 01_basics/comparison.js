78804259467b2ecf20d355fb558ffc84f325063d
gargeti <138214793+gargeti@users.noreply.github.com>
2024-10-17 20:05:40 +0000

    changes

--- 01_basics/comparison.js
+++ 01_basics/comparison.js
@@ -14,11 +14,14 @@
 // the reason is that an equalitycheck ==and comparisons><>=<=work differently .
 // comparisons convert null to a number,treated it as 0.
 // that's why null>=0 is true and null>0 is false.
+// comparisons of null and the undefined are struck us in a dilema to answer the given compare answer so we just take care while doing this type of conversion .
+// we will avoid this and make sure that we written a clean code in the programing.
+
 console.log(undefined==0);//false
 console.log(undefined>=0);//false
 console.log(undefined<=0);//false
 console.log(undefined!=0);//true
-
+// strict check for checking the strictly variables along with their data type as well with ===(triple equals)
 
 // strict check for checking the strictly variables along with their data type as well with ===(triple eqalals to)
-console.log("")
+console.log("");
