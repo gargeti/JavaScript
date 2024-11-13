0b63c0b29907d0d01a3bd5ace6ea57fc576ecbd2
gargeti <138214793+gargeti@users.noreply.github.com>
2024-11-13 21:44:31 +0000

    objects done

--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -31,4 +31,9 @@ const obj2={3:"a",4:"b"}
 
 
 const obj3={...obj1,...obj2}
-console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
