83a8f6d3cda7bafac7d931ea67c593596c98c4bd
gargeti <138214793+gargeti@users.noreply.github.com>
2024-11-13 21:26:56 +0000

    objescts

---
+++ 02_basics/objects2.js
@@ -0,0 +1,34 @@
+// const tinderUser=new Object()//singleton object .
+const tinderUser={}//non-singleton object.
+ tinderUser.id="Garg18"
+ tinderUser.name="Eti Garg"
+ tinderUser.isLoggedIn = false
+// console.log(tinderUser);//{}
+
+
+const regularUser={
+    email: "gargeti18@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"eti",
+            lastname:"garg"
+        }
+    }
+}
+
+// console.log(regularUser.fullname);//{ userfullname: { firstname: 'eti', lastname: 'garg' } }
+// console.log(regularUser.fullname.userfullname.firstname);//eti
+
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+// const obj3={obj1,obj2}
+// console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+// const obj3=Object.assign({},obj1,obj2)
+// console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// as the assign operator is doing that the object be merge with the target and the source and it will take a target and others are the data that has been transfer to it then it will take a empty target that the whole objects copied in it.
+
+
+const obj3={...obj1,...obj2}
+console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
