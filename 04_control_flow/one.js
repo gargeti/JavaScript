f8336f41de4adaf4a736296a6d304720f4d01d4b
gargeti <138214793+gargeti@users.noreply.github.com>
2024-11-27 21:12:11 +0000

    if statements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+//IF STATEMENT
+// if(condition=>true){
+// statements of execution
+// }
+
+
+const temp = 50
+
+if(temp>=50){
+    console.log("greater than and equals to 50");
+}
+else{
+    console.log("less than 50");
+}
+console.log("successful execution of code");
+//greater than and equals to 50
+// successful execution of code
+
+
+
+
+// *******************operators*************
+// <lessthan
+// >greaterthan
+// >=greaterthanequalsto
+// <=lessthanequalsto
+// ==equals to
+// !=notequalto
+// ===strictequalto with data type too
