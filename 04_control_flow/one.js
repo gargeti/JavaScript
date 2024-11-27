1fbc1b4e150167295e80de2a0137604ae35bc1b9
gargeti <138214793+gargeti@users.noreply.github.com>
2024-11-27 21:22:54 +0000

    const,var and let in if

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -4,17 +4,48 @@
 // }
 
 
-const temp = 50
-
-if(temp>=50){
-    console.log("greater than and equals to 50");
-}
-else{
-    console.log("less than 50");
-}
-console.log("successful execution of code");
+// const temp = 50
+// if(temp>=50){
+//     console.log("greater than and equals to 50");
+// }
+// else{
+//     console.log("less than 50");
+// }
+// console.log("successful execution of code");
 //greater than and equals to 50
 // successful execution of code
+
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+// //power is not defined
+
+
+
+// const score=200
+// if(score>100){
+//     var power="fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+// user power: fly
+// user power: fly(scope of var is completely global that can acccessed globally in the whole program so the execution of power be done outside the control statement.)
+
+
+
+
+const score=200
+if(score>100){
+    let power="fly"
+    console.log(`user power: ${power}`);
+}
+console.log(`user power: ${power}`);
+// power is not defined
+
 
 
 
