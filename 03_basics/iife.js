e57fbd09f2227585535dede05855c2e071fe8c29
gargeti <138214793+gargeti@users.noreply.github.com>
2024-11-27 20:03:58 +0000

    iffe

--- 03_basics/iife.js
+++ 03_basics/iife.js
@@ -6,5 +6,26 @@
 // chai()
 
 (function chai(){
+    //named iffe
     console.log(`db connected`);//db connected
-})()
+})();
+// use (;)for end the iffe function and starts a new.
+
+(  ()=> {
+    //unamed iffe
+    console.log(`db connected again`);//db connected again
+})();
+
+(  (name)=> {
+    //argumented iffe
+    console.log(`db connected ${name}`);//db connected again
+})("eti garg");
+
+
+
+// ()()
+//first bracket =>defination of function and second bracket => execution call of function.
+
+
+// iife functions are those which execute immediately 
+// sometimes global scope pollution occur so to reduce the global variables or functions polltuion we use the iife function.
