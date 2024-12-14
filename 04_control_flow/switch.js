ad76ee151348310ea0ef375fede080230318acb2
gargeti <138214793+gargeti@users.noreply.github.com>
2024-12-14 14:10:59 +0000

    switch cases

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// switch(key){
+//     case value:
+//         break;
+
+//    default:
+//     break;
+
+// }
+
+// const month=3
+// switch(month){
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("mar");
+//         break;
+//     case 4:
+//         console.log("apr");
+//         break;
+//     default:
+//         console.log("no month match");
+//         break;
+// }
+const mon="feb"
+switch(mon){
+    case "jan":
+        console.log(1);
+        break;
+    case "feb":
+        console.log(2);
+        break;
+    case "march":
+        console.log(3);
+        break;
+    case "april":
+        console.log(4);
+        break;
+    default:
+        console.log("no month match");
+        break;
+}
+// break statement use to break the control flow and then after that no execution fron the switch will bw counter 
+// if break statement is not in case so cases after a sucessful match cases will be executed except default
+
